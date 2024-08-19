372ef5bc12a6a4e45567daaea972c1134ad0da11
RCNR <iop04226@gmail.com>
2024-08-19 17:21:45 +0900

    블로그 Articles 목록 뷰 반환 코드(리스트 반환)

--- src/main/java/alone_project/alone/controller/BlogViewController.java
+++ src/main/java/alone_project/alone/controller/BlogViewController.java
@@ -1,11 +1,29 @@
 package alone_project.alone.controller;
 
+import alone_project.alone.dto.ArticleListViewResponse;
+import alone_project.alone.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BlogViewController {
 
+    private final BlogService blogService;
 
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
 }
