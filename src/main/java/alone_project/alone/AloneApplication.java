668c69c8e3cf3ef2d21a2053415e4da1b2e60523
RCNR <iop04226@gmail.com>
2024-08-21 18:29:06 +0900

    created_time, updated_time 자동으로 업데이트 될 수 있게 어노테이션 추가

--- src/main/java/alone_project/alone/AloneApplication.java
+++ src/main/java/alone_project/alone/AloneApplication.java
@@ -2,7 +2,9 @@ package alone_project.alone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // created_time, updated_time 자동으로 업데이트
 @SpringBootApplication
 public class AloneApplication {
 
