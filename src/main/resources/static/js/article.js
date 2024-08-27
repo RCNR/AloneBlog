444df72fb4a465139671b40dc3f6a1e6b3be236a
RCNR <iop04226@gmail.com>
2024-08-27 17:15:50 +0900

    글 생성을 위한 js 코드 추가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -18,3 +18,26 @@ if (deleteButton) {
         } 
     });
 }
+
+
+// 생성
+const createButton = document.getElementById("create-btn");
+
+if (createButton) {
+
+    createButton.addEventListener("click", (event) => {
+        fetch("/api/articles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value,
+            })
+        }).then(() => {
+            alert("글 등록이 완료되었습니다.");
+            location.replace("/articles")
+        });
+    })
+}
