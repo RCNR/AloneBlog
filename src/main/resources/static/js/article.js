5e6812164df013796033155537cd306262fa7e9c
RCNR <iop04226@gmail.com>
2024-08-27 13:24:24 +0900

    주석 삭제

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,18 +1,6 @@
 // 삭제
 const deleteButton = document.getElementById('delete-btn');
 
-// if (deleteButton) {
-//     deleteButton.addEventListener('click', event => {
-//         let id = document.getElementById('article-delete-id').value;
-//         fetch(`/api/articles/${id}`, { // BlogApiController의 DeleteMapping이 실행된다.
-//             method: 'DELETE'
-//         })
-//             .then(() => {
-//                 alert("삭제 완료 되었습니다.");
-//                 location.replace('/articles');
-//             })
-//     });
-// }
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
 
