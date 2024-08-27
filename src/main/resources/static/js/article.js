787dfadca9e4854d1e70abf92426f39b28962c3f
RCNR <iop04226@gmail.com>
2024-08-27 13:01:16 +0900

    알기 쉽게 article-delete-id 로 변경

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -3,7 +3,7 @@ const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
-        let id = document.getElementById('article-id').value;
+        let id = document.getElementById('article-delete-id').value;
         fetch(`/api/articles/${id}`, { // BlogApiController의 DeleteMapping이 실행된다.
             method: 'DELETE'
         })
